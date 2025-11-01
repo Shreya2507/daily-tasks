29a8796c803422439b230c3f936718639492789c
Shreya Mathur <smathur245@gmail.com>
2025-11-01 22:45:33 +0530

    completed

--- day-4/main.js
+++ day-4/main.js
@@ -92,7 +92,7 @@ console.log(extractAndDisplay());
 //Q10.
 const objArr = [
     {
-        "id": 1,
+        // "id": 1,
         "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
         "price": 109.95,
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
@@ -105,9 +105,38 @@ const objArr = [
     }
 ]
 
-// const {"id", "title", "rate", "count"} = 
+//USING hasOwn() ---
+// const getSelectedKeys = (arr) => {
+//     const selectedKeys = arr.map((obj) => {
+//         return {
+//          "id": Object.hasOwn(obj, "id") ? obj.id : undefined,
+//          "title": Object.hasOwn(obj, "title") ? obj.title : undefined,
+//          "rate": obj.rating.rate, 
+//          "count": obj.rating.count
+//         }
+//     });
+
+//     return selectedKeys;
+// }
+
+//USING destructuring ---
+const getSelectedKeys = (arr) => {
+    const selectedKeys = arr.map(({id, title, rating}) => {
+        return {
+         "id": id,
+         "title": title,
+         "rate": rating?.rate, 
+         "count": rating.count
+        }
+    });
+
+    return selectedKeys;
+}
+
+console.log(getSelectedKeys(objArr));
 
 //Q11.
+// empty string is falsy value
  const fun = (name = "abc") => {
         if (name) {
           console.log("if", name);
@@ -121,8 +150,65 @@ fun("");
 
 //Q12.
 const obj = { a: 1, b: { c: 2 } };
-const obj2 = JSON.parse(JSON.stringify(obj));
-console.log(obj2);
+
+//using JSON.parse() and JSON.stringify()
+// const obj2 = JSON.parse(JSON.stringify(obj));
+
+function getDeepClone(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  let clone = {};
+  for (let key in obj) {
+    clone[key] = getDeepClone(obj[key]);
+  }
+
+  return clone;
+}
+
+const deepClone = getDeepClone(obj);
+
+console.log("Original - " + JSON.stringify(obj));
+console.log("Clone - " + JSON.stringify(deepClone));
+deepClone.a = 10;
+deepClone.b.c = 20
+console.log('After change :');
+console.log("Original - " + JSON.stringify(obj));
+console.log("Clone - " + JSON.stringify(deepClone));
+
+
+//Q13.
+let flattenArray = (inputArray) => {
+    let outputArray = [];
+
+    recursion(inputArray, outputArray, 0);
+    
+    return outputArray;
+}
+
+let recursion = (inputArray, outputArray, index) => {
+    //base condition -- only run until index reaches end of array
+    if(index >= inputArray.length) {
+        return;
+    }
+
+    const elem = inputArray[index];
+
+    //if the element at index, is array
+    if(Array.isArray(elem)){
+        recursion(elem, outputArray, 0); //send elem(array) to be processed
+    }
+    else{
+        outputArray.push(elem); //if the element is single value, push directly
+    }
+
+    //send next element for processing
+    recursion(inputArray, outputArray, index+1);
+}
+
+const nestedArr = [1, [2, [3, [4, 5]]]];
+console.log(flattenArray(nestedArr));
 
 
 
